d870aaea75f84d561d8a262b0fd452d5213d602e
swaami86 <swami09991@outlook.com>
2025-09-28 09:02:07 +0530

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,4 @@
+// This file tells Cloudflare that this is a Workers project
+// It imports and exports the worker from the workers/index.js file
+import worker from './workers/index.js';
+export default worker;
